cb25cfa0d7bdc98a06e37bf3e62f9c70a72a3a1f
Varun Gohe <07varun70@gmail.com>
2024-07-28 15:41:04 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class QRScannerScreen extends StatefulWidget {
   const QRScannerScreen({super.key});
 
   @override
+  //checking
   // ignore: library_private_types_in_public_api
   _QRScannerScreenState createState() => _QRScannerScreenState();
 }
